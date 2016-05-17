34272c89c2ed3233d13999190e56f9add44386f0
Kyle Burke <kyle@massroots.com>
2016-05-16 21:52:49 -0600

    first commit

---
+++ src/service.js
@@ -0,0 +1,61 @@
+(function() {
+  'use strict';
+  angular
+    .module('$localStorage', [])
+    .factory('$localStorage', $localStorage);
+  $localStorage.$inject = ['$window', '$timeout'];
+  /* @ngInject */
+  function $localStorage($window, $timeout) {
+    var service = {
+      get: get,
+      set: set,
+      checkExpiredItems: checkExpiredItems
+    };
+    return service;
+    ////////////////
+    function get(key) {
+      return $window.localStorage.getItem(key);
+    }
+
+    function set(key, value, expiration) {
+      if (expiration !== undefined && Date.parse(expiration) === NaN) {
+        throw new Error('Invalid timestamp string given');
+        return false;
+      }
+
+      $window.localStorage.setItem(key, value);
+
+      if (expiration !== undefined) {
+        $window.localStorage.setItem(key+'-expiration', expiration);
+        _registerExpiration(key, expiration);
+      }
+    }
+
+    function remove(key) {
+      $window.localStorage.removeItem(key);
+      $window.localStorage.removeItem(key+'-expiration');
+    }
+
+    function checkExpiredItems() {
+      for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+
+        if (key.indexOf('-expiration') > -1) {
+          _registerExpiration(key, localStorage.getItem(key+'-expiration'));
+        }
+      }
+    }
+
+    function _registerExpiration(key, expiration) {
+      var timeLeft = expiration - new Date();
+      if (timeLeft <= 0) {
+        remove(key);
+      }
+      else {
+        setTimeout(function() {
+          remove(key);
+        }, timeLeft);
+      }
+    }
+  }
+})();
---
+++ test/service.spec.js
@@ -0,0 +1,36 @@
+describe('$localStorage', function() {
+  var $localStorage, $window;
+
+  beforeEach(module('$localStorage'));
+  beforeEach(inject(function(_$localStorage_, _$window_) {
+    $localStorage = _$localStorage_;
+    $window = _$window_;
+  }));
+
+
+  it('should set an item correctly', function() {
+    $localStorage.set('test', 'value');
+    expect($window.localStorage.getItem('test')).toEqual('value');
+  });
+
+  it('should set an item with an expiration', function() {
+    var expiration = Date.now() + 100;
+    $localStorage.set('test', 'value', expiration);
+
+    expect($window.localStorage.getItem('test-expiration')).not.toBe(null);
+  });
+
+  it('should remove an item after it has expired', function() {
+    jasmine.clock().install();
+
+    var expiration = Date.now() + 10;
+    $localStorage.set('test', 'value', expiration);
+
+    jasmine.clock().tick(11);
+
+    expect($window.localStorage.getItem('test')).toBe(null);
+    expect($window.localStorage.getItem('test-expiration')).toBe(null);
+
+    jasmine.clock().uninstall();
+  })
+})
